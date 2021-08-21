d9da235aa9c7252ab8fcdbf9641931f4893da9e2
mrvinil <mrvinil2061@gmail.com>
2021-08-21 17:39:55 +0300

    finish.v2-drf_yasg

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CustomUserViewSet, IngredientsViewSet, RecipeViewSet,
-                    TagViewSet)
+from .views import (IngredientsViewSet, RecipeViewSet, TagViewSet)
 
 app_name = 'recipes'
 
