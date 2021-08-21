4c1f75073a56b91047373ef52a7062420d6d36f5
mrvinil <mrvinil2061@gmail.com>
2021-08-21 18:48:13 +0300

    finish.v2-urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,21 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from djoser import views as djoser_views
-from backend.backend.routers import v1_router
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(v1_router.urls)),
-    path('api/', include('djoser.urls')),
-    path(
-        'api/auth/token/login/',
-        djoser_views.TokenCreateView.as_view(),
-        name='login'
-    ),
-    path(
-        'api/auth/token/logout/',
-        djoser_views.TokenDestroyView.as_view(),
-        name='login'
-    ),
+    path('api/auth/', include('djoser.urls.authtoken')),
+    path('api/', include('recipes.urls')),
 ]
---
+++ backend/recipes/urls.py
@@ -0,0 +1,18 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CustomUserViewSet, IngredientsViewSet, RecipeViewSet,
+                    TagViewSet)
+
+app_name = 'recipes'
+
+router = DefaultRouter()
+
+router.register('tags', TagViewSet, basename='tags')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('ingredients', IngredientsViewSet, basename='ingredients')
+router.register('users', CustomUserViewSet, basename='users')
+
+urlpatterns = [
+    path('v1/', include(router.urls))
+]
