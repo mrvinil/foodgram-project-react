0971fe37edc0a846d3bbf180f47447bc8eb2c316
mrvinil <mrvinil2061@gmail.com>
2021-08-21 17:06:00 +0300

    finish.v2-drf_yasg

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,22 +12,22 @@ urlpatterns = [
 ]
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Foodgram API",
-      default_version='v1',
-      description="Документация для проекта Foodgram",
-      contact=openapi.Contact(email="admin@walmarkt.ru"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Foodgram API",
+        default_version='v1',
+        description="Документация для проекта Foodgram",
+        contact=openapi.Contact(email="admin@walmarkt.ru"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
