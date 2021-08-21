2601e6b69639c3c029932771d47f30186174efe2
mrvinil <mrvinil2061@gmail.com>
2021-08-21 17:39:55 +0300

    finish.v2-drf_yasg

---
+++ backend/backend/routers.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from backend.recipes.views import (CustomUserViewSet, IngredientsViewSet,
+                                   RecipeViewSet, TagViewSet)
+
+v1_router = DefaultRouter()
+
+v1_router.register(r'tags', TagViewSet, basename='tags')
+v1_router.register(r'recipes', RecipeViewSet, basename='recipes')
+v1_router.register(r'ingredients', IngredientsViewSet, basename='ingredients')
+v1_router.register(r'users', CustomUserViewSet, basename='custom-users')
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
 from django.urls import include, path
+from djoser import views as djoser_views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.conf.urls import url
+from routers import v1_router
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include('recipes.urls')),
+    path('api/', include(v1_router.urls)),
+    path('api/', include('djoser.urls')),
+    path(
+        'api/auth/token/login/',
+        djoser_views.TokenCreateView.as_view(),
+        name='login'
+    ),
+    path(
+        'api/auth/token/logout/',
+        djoser_views.TokenDestroyView.as_view(),
+        name='login'
+    ),
 ]
 
 schema_view = get_schema_view(
