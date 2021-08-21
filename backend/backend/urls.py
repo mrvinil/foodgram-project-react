47d1bbc46f0e9be72dc1b00114f2ce5151d5b8ae
mrvinil <mrvinil2061@gmail.com>
2021-08-21 18:11:21 +0300

    finish.v2-drf_yasg2

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from djoser import views as djoser_views
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from django.conf.urls import url
-from routers import v1_router
+from backend.backend.routers import v1_router
 
 
 urlpatterns = [
@@ -22,25 +18,4 @@ urlpatterns = [
         djoser_views.TokenDestroyView.as_view(),
         name='login'
     ),
-]
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Foodgram API",
-        default_version='v1',
-        description="Документация для проекта Foodgram",
-        contact=openapi.Contact(email="admin@walmarkt.ru"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns += [
-    url(r'^swagger(?P<format>\.json|\.yaml)$',
-        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui'),
-    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'),
 ]
